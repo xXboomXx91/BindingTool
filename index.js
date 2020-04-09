fbaf9dd1c022516ea9bc5c8dec14cee2ff39b06f
xXboomXx91 <42394278+xXboomXx91@users.noreply.github.com>
2020-04-09 14:33:26 -0600

    two more input fields

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Sqrt(((((diameter - seamAlow)* pi)/strips)^2)/2)
 function calculateBiasCircumfrence() {
   var diameter = document.getElementById("Diameter").value;
+  var seamAlow = document.getElementById("seamAlow").value;
+  var stripNum = document.getElementById("stripNum").value;
   var out = Math.sqrt(
-    Math.pow(((parseFloat(diameter) - 0.25) * 3.14) / 7, 2) / 2
+    Math.pow(
+      ((parseFloat(diameter) - parseFloat(seamAlow)) * 3.14) /
+        parseFloat(stripNum),
+      2
+    ) / 2
   );
   document.getElementById("Output").innerHTML = out;
   console.log(out);
